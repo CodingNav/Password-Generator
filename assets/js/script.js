6171b989cb99feace762a46a6d073c47ce0968d7
CodingNav <navleenkaurr@gmail.com>
2021-06-22 20:06:42 -0400

    Created for loop for lowercase letters

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,21 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
+function generatePassword() {
+
 var length = prompt("Enter the length of the password you want.");
-var lowercaseLetters =  confirm("Do you want to include lowercase letters in your password?");
-var uppercaseLetters =  confirm("Do you want to include uppercase letters in your password?");
-var numbers =  confirm("Do you want to include numbers in your password?");
-var specialCharacters =  confirm("Do you want to include specialCharacters in your password?");
+var lowercaseMessage =  confirm("Do you want to include lowercase letters in your password?");
+var uppercaseMessage =  confirm("Do you want to include uppercase letters in your password?");
+var numbersMessage =  confirm("Do you want to include numbers in your password?");
+var specialMessage =  confirm("Do you want to include special characters in your password?");
+var password = "";
+
+for (var i = 0; i < length; i++) {
+    var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+    var lowerCharacter = lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+    password += lowerCharacter;
+}
+
+return password;
+
+}
