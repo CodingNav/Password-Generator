6516bd50d1cc0a7d1169f5fe6580278c65fc1736
CodingNav <navleenkaurr@gmail.com>
2021-06-23 12:40:24 -0400

    Added if statements for the password criterias

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
@@ -16,19 +16,39 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
 
-var length = prompt("Enter the length of the password you want.");
-var lowercaseMessage =  confirm("Do you want to include lowercase letters in your password?");
-var uppercaseMessage =  confirm("Do you want to include uppercase letters in your password?");
-var numbersMessage =  confirm("Do you want to include numbers in your password?");
-var specialMessage =  confirm("Do you want to include special characters in your password?");
-var password = "";
+    var length = prompt("Enter the length of the password you want.");
+    var lowercaseMessage = confirm("Do you want to include lowercase letters in your password?");
+    var uppercaseMessage = confirm("Do you want to include uppercase letters in your password?");
+    var numbersMessage = confirm("Do you want to include numbers in your password?");
+    var specialMessage = confirm("Do you want to include special characters in your password?");
+    var allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var password = "";
 
-for (var i = 0; i < length; i++) {
-    var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
-    var lowerCharacter = lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
-    password += lowerCharacter;
+
+    if (lowercaseMessage == true) {
+        var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+        allowedCharacters += lowerLetters;
+    }
+    if (uppercaseMessage == true) {
+        var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        allowedCharacters += upperLetters;
+    }
+    if (numbersMessage == true) {
+        var numbers = "0123456789";
+        allowedCharacters += numbers;
+    }
+    if (specialMessage == true) {
+        var specialCharacter = " !#$%&()*+,=./:;<=>?@[\\]\"\'^_`{|}~"
+        allowedCharacters += specialCharacter;
+    }
+
+    for (var i = 0; i < length; i++) {
+
+        var lowerCharacter = lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+        password += lowerCharacter;
+    }
+    return password;
+
 }
 
-return password;
 
-}
